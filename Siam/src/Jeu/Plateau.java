93f9740380e47cb92818b8c35bfbc7bd8cefa5f0
Etienne <Etienne@10.4.183.218>
2015-04-08 11:24:12 +0200

    test 4

--- Siam/src/Jeu/Plateau.java
+++ Siam/src/Jeu/Plateau.java
@@ -5,21 +5,39 @@
  */
 package Jeu;
 
-
-
 /**
  *
  * @author Etienne
  */
 public class Plateau {
+
     private Case[][] casePlateau;
     private Case[] caseRhino;
     private Case[] caseElephant;
-    
-    public Plateau(Case[][] casePlateau,Case[] caseRhino,Case[] caseElephant){
-        this.casePlateau=casePlateau;
-        this.caseRhino=caseRhino;
-        this.caseElephant=caseElephant;
+
+    public Plateau(int x, int y, int nbRhyno, int nbEleph) {
+        for (int i=0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                if(i!=0 && i!=x-1 && j==(y/2)){
+                this.casePlateau[i][j] = new Case(i,j,3);
+                System.out.println("Creation d'une montagne en "+i +"-"+j);
+                }
+                else{
+                this.casePlateau[i][j] = new Case(i,j,4);
+                System.out.println("Creation d'une case vide en "+i +"-"+j);
+                }
+                
+            }
+        }
+        for (int i=0; nbRhyno < x; i++) {
+        this.caseRhino[i] = new Case(i,0,0);
+        System.out.println("Creation du "+i +"eme Rhyno");
+        }
+        for (int i=0; nbEleph < x; i++) {
+        this.caseElephant[i] = new Case(i,0,1);
+        System.out.println("Creation du "+i +"eme Eleph");
+        }
+        
     }
 
     /**
